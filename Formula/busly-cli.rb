6d2b9a081f616476fe9a3423e9c328c92fb0a762
Michael Fyffe <6224270+TraGicCode@users.noreply.github.com>
2026-06-10 07:51:34 +1000

    busly-cli 0.64.10
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/busly-cli.rb
+++ Formula/busly-cli.rb
@@ -1,9 +1,9 @@
 class BuslyCli < Formula
   desc "Unofficial CLI for NServiceBus"
   homepage "https://tragiccode.com/busly-cli/"
-  url "https://github.com/TraGicCode/busly-cli/releases/download/v0.64.9/busly-cli-v0.64.9-osx-arm64.tar.gz"
-  version "0.64.9"
-  sha256 "1599cacc1d7d5aaf031b13d2b3f3f9aad146978aba00de478a4e58bfa67dc7a1"
+  url "https://github.com/TraGicCode/busly-cli/releases/download/v0.64.10/busly-cli-v0.64.10-osx-arm64.tar.gz"
+  version "0.64.10"
+  sha256 "0bb192e11a873f996bac754703052238fef337dd9dd8aa0c442d9216bccc5d19"
   license "Apache-2.0"
 
 
